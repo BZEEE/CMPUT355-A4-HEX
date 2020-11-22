444b0651e7a42aa3ebe1747203bff36a5b81e4c9
Nectarios Chroniaris <nchroniaris@gmail.com>
2020-11-21 21:10:13 -0700

    update formatting

--- src/app/hex-visualizer/hex-visualizer.component.ts
+++ src/app/hex-visualizer/hex-visualizer.component.ts
@@ -1,55 +1,56 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
-import { CanvasSingleton } from './board/CanvasSingleton';
-import { HexGrid } from './board/HexGrid';
+import {CanvasSingleton} from './board/CanvasSingleton';
+import {HexGrid} from './board/HexGrid';
 
 @Component({
-  selector: 'app-hex-visualizer',
-  templateUrl: './hex-visualizer.component.html',
-  styleUrls: ['./hex-visualizer.component.css']
+    selector: 'app-hex-visualizer',
+    templateUrl: './hex-visualizer.component.html',
+    styleUrls: ['./hex-visualizer.component.css']
 })
 export class HexVisualizerComponent implements OnInit {
-  @ViewChild('canvas', {static: true}) canvasRef: ElementRef<HTMLCanvasElement>;
-  hexGrid: HexGrid;
-  animationId: number;
+    @ViewChild('canvas', {static: true}) canvasRef: ElementRef<HTMLCanvasElement>;
+    hexGrid: HexGrid;
+    animationId: number;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-    // set Canvas Singleton
-    CanvasSingleton.getInstance().setContext(this.canvasRef.nativeElement);
-    CanvasSingleton.getInstance().getCanvas().width = Math.round(window.innerWidth * 0.8)
-    CanvasSingleton.getInstance().getCanvas().height = window.innerHeight;
-    CanvasSingleton.getInstance().getCanvas().style.backgroundColor = "#db591d";
-  }
+    ngOnInit(): void {
+        // set Canvas Singleton
+        CanvasSingleton.getInstance().setContext(this.canvasRef.nativeElement);
+        CanvasSingleton.getInstance().getCanvas().width = Math.round(window.innerWidth * 0.8);
+        CanvasSingleton.getInstance().getCanvas().height = window.innerHeight;
+        CanvasSingleton.getInstance().getCanvas().style.backgroundColor = '#db591d';
+    }
 
-  startGame() {
-    this.hexGrid = new HexGrid();
-    this.animationLoop()
-  }
+    startGame() {
+        this.hexGrid = new HexGrid();
+        this.animationLoop();
+    }
 
-  stopGame() {
-    let canvas = CanvasSingleton.getInstance().getCanvas();
-    CanvasSingleton.getInstance().getContext().clearRect(0, 0, canvas.width, canvas.height);
-    window.cancelAnimationFrame(this.animationId);
-  }
+    stopGame() {
+        let canvas = CanvasSingleton.getInstance().getCanvas();
+        CanvasSingleton.getInstance().getContext().clearRect(0, 0, canvas.width, canvas.height);
+        window.cancelAnimationFrame(this.animationId);
+    }
 
-  animationLoop() {
-    // clear the canvas
-    let canvas = CanvasSingleton.getInstance().getCanvas();
-    CanvasSingleton.getInstance().getContext().clearRect(0, 0, canvas.width, canvas.height);
-    // render the hex grid
-    this.hexGrid.renderGrid();
-    // call next animation loop
-    this.animationId = window.requestAnimationFrame(this.animationLoop.bind(this))
+    animationLoop() {
+        // clear the canvas
+        let canvas = CanvasSingleton.getInstance().getCanvas();
+        CanvasSingleton.getInstance().getContext().clearRect(0, 0, canvas.width, canvas.height);
+        // render the hex grid
+        this.hexGrid.renderGrid();
+        // call next animation loop
+        this.animationId = window.requestAnimationFrame(this.animationLoop.bind(this));
 
-  }
+    }
 
-  mouseClickOnCanvas(clickEvent: MouseEvent) {
-    // get x, y coordinates of click relative to canvas coordinate system (0, 0) = top-left corner
-    this.hexGrid.checkMouseClick(clickEvent.offsetX, clickEvent.offsetY);
-    // check for win state after previous input
-    this.hexGrid.checkWinState();
-  }
+    mouseClickOnCanvas(clickEvent: MouseEvent) {
+        // get x, y coordinates of click relative to canvas coordinate system (0, 0) = top-left corner
+        this.hexGrid.checkMouseClick(clickEvent.offsetX, clickEvent.offsetY);
+        // check for win state after previous input
+        this.hexGrid.checkWinState();
+    }
 
 }
